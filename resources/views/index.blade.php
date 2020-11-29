0e4c3ca463359c6a35a6d6f3c79ed6826f1e9d8d
ManishRatoori <prince7500ratudi@gmail.com>
2020-11-29 13:27:37 +0530

    taskdone

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+
+
+<body>
+<form action="https://api.cloudconvert.com/convert" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="hidden" name="apikey" value="gjA3zVu1iRUOkH9WYIsAqAAy8fGmGE719HQjmrghEeJW3yc289Wxuj6JleAm9wyg">
+    <input type="hidden" name="inputformat" value="doc">
+    <input type="hidden" name="outputformat" value="pdf">
+    <input type="hidden" name="input" value="upload">
+    <input type="hidden" name="filename" value="document.doc">
+    <input type="hidden" name="timeout" value="400">
+    <input type="hidden" name="wait" value="true">
+    <input type="hidden" name="download" value="true">
+    <input type="hidden" name="save" value="true">
+    <input type="submit" value="Convert!">
+</form>
+</body>
